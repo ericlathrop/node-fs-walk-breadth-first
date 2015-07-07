19276d7dbb885d77d1ef3b3ff919797546cba780
Eric Lathrop <eric@ericlathrop.com>
2015-07-07 19:28:10 -0400

    Stat files and call iterator

--- test/fs-walk-parallel-test.js
+++ test/fs-walk-parallel-test.js
@@ -2,16 +2,16 @@
 
 var fsWalkParallel = require("../lib/fs-walk-parallel");
 var mockFs = require("mock-fs");
-var tap = require("tap");
+var test = require("tape");
 
-function setup(t, fs) {
-	t.tearDown(mockFs.restore.bind(mockFs));
-	mockFs(fs);
-	t.plan(1);
+function setup(t, fakeFs) {
+	mockFs.restore();
+	mockFs(fakeFs);
 }
 
-tap.test("nonexistant folder should return error", function(t) {
+test("nonexistant folder should return error", function(t) {
 	setup(t, {});
+	t.plan(1);
 	fsWalkParallel("doesntExist", function() {
 		throw "should never happen";
 	}, function(err) {
@@ -19,11 +19,21 @@ tap.test("nonexistant folder should return error", function(t) {
 		t.end();
 	});
 });
-
-tap.test("empty folder should return no errors", function(t) {
+test("empty folder should return no errors", function(t) {
 	setup(t, { "folder": {} });
+	t.plan(1);
 	fsWalkParallel("folder", function() {
 		throw "should never happen";
+	}, function(err) {
+		t.notOk(err, "should have no errors");
+		t.end();
+	});
+});
+test("single file should call iterator once with stat", function(t) {
+	setup(t, { "folder": { "file": "contents" } });
+	t.plan(2);
+	fsWalkParallel("folder", function(stat) {
+		t.ok(stat.isFile(), "should return a fs.Stats");
 	}, function(err) {
 		t.notOk(err, "should have no errors");
 		t.end();

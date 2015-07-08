de90702bf7141e3f08c5f619e64c5abc1e330abd
Eric Lathrop <eric@ericlathrop.com>
2015-07-07 20:00:22 -0400

    Send filename to iterator

--- test/fs-walk-parallel-test.js
+++ test/fs-walk-parallel-test.js
@@ -29,11 +29,21 @@ test("empty folder should return no errors", function(t) {
 		t.end();
 	});
 });
+test("one file should call iterator once with path", function(t) {
+	setup(t, { "folder": { "file": "contents" } });
+	t.plan(2);
+	fsWalkParallel("folder", function(filename) {
+		t.equal(filename, "folder/file", "should return the file's path");
+	}, function(err) {
+		t.notOk(err, "should have no errors");
+		t.end();
+	});
+});
 test("two files should call iterator twice with stat", function(t) {
 	setup(t, { "folder": { "file1": "contents1", "file2": "contents2" } });
 	t.plan(3);
-	fsWalkParallel("folder", function(stat) {
-		t.ok(stat.isFile(), "should return a fs.Stats");
+	fsWalkParallel("folder", function(filename, stat) {
+		t.equal(stat.isFile(), true, "should return a fs.Stats");
 	}, function(err) {
 		t.notOk(err, "should have no errors");
 		t.end();
